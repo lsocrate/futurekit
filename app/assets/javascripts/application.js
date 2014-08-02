bcaa6c8b7bd27456b14542cc474cc5cfb4c3d068
Alexander K <knuxus@gmail.com>
2014-08-02 02:25:17 +0100

    added the modal thingy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+
+  var clickOnPopupLink = function(){
+    $('body').on('click', '.static-popup-link', function(){
+      $('#myModal').modal('show');
+    });
+  }
+
+  clickOnPopupLink();
+
+});
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -14,6 +14,7 @@ class CardsController < ApplicationController
   # GET /cards/1
   # GET /cards/1.json
   def show
+    respond_to :js
   end
 
   # GET /cards/new
