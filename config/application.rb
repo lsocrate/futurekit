01e7237619c987f23653b8972d1aab5e1944efd2
Alexander K <knuxus@gmail.com>
2014-08-03 23:33:34 +0100

    globalize is working for categories

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Futurekit
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**','*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
     config.i18n.enforce_available_locales = false
-
     config.i18n.fallbacks = true
 
   end
