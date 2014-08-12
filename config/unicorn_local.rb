e6111afd8213a7c7d1b067dace3e1dce6efb8c30
Rui Mochila <rui.mochila@gmail.com>
2014-08-12 18:29:13 +0100

    Some configuration files for production nginx and unicorn

--- config/unicorn_local.rb
+++ config/unicorn_local.rb
@@ -1,10 +1,3 @@
-root = "/home/inspiring/apps/futurekit"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-# stderr_path "#{root}/log/unicorn.log"
-# stdout_path "#{root}/log/unicorn.log"
-listen "/tmp/unicorn.futurekit.sock"
-
 #worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
 worker_processes 2
 timeout 30
@@ -17,7 +10,7 @@ before_exec do |_|
 end
 
 before_fork do |server, worker|
-  # @sidekiq_pid ||= spawn("bundle exec sidekiq -d -c 2 -L #{root}/log/sidekiq.log -e production")
+  # @sidekiq_pid ||= spawn("bundle exec sidekiq -d -c 2 -L log/sidekiq.log -e production")
   
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
@@ -28,6 +21,7 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 end
 
+
 after_fork do |server, worker|
   # Sidekiq.configure_client do |config|
   #   config.redis = { :size => 1 }
@@ -36,8 +30,9 @@ after_fork do |server, worker|
   #   config.redis = { :size => 5 }
   # end
 
-  child_pid = server.config[:pid].sub(/pid$/, "worker.#{worker.nr}.pid")
-  system("echo #{Process.pid} > #{child_pid}")
+  
+  # child_pid = server.config[:pid].sub(/pid$/, "worker.#{worker.nr}.pid")
+  # system("echo #{Process.pid} > #{child_pid}")
 
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
