ea0f0dec967ade9852e1822bbded2a0cb7e769ee
Rui Mochila <rui.mochila@gmail.com>
2014-08-13 11:34:15 +0100

    Little thing missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,8 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   before_save { email.downcase! }
-
+  before_create :create_remember_token
+  
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
